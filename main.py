dcdb2ea4ce94dd70f088dcf09a23e4af54f709b0
hpc19032 <144771378+hpc19032@users.noreply.github.com>
2023-10-16 04:15:51 +0000

    Working on big fixes with input box

--- main.py
+++ main.py
@@ -2,17 +2,13 @@
 import sys
 import pygame
 import time
-from pygame.locals import QUIT, KEYDOWN, K_RETURN, K_BACKSPACE
-import pygame.font
-import pygame_textinput  
 
 # Initializing pygame
 pygame.init()
 pygame.font.init()
 
 # Setting screen
-screen = pygame.display.set_mode((280, 343))
-screen.fill((243,255,255))
+screen = pygame.display.set_mode([280, 343])
 
 #Loading images
 bg_surface = pygame.image.load('Assets/Background.png')
@@ -28,69 +24,19 @@ lobby_map = pygame.image.load('Assets/Lobby.png')
 
 #Setting title
 pygame.display.set_caption("Down to Freedom")
+screen.blit(bg_surface, (0, 0))
+screen.fill((243,255,255))
 
 #settinginput box variables
-font = pygame.font.SysFont("dejavusans", 13)  
-global textoutput
-textoutput = ""
-textinput = pygame_textinput.TextInputVisualizer(font_object=font)
+font = pygame.font.SysFont("dejavusans", 20)  
 clock = pygame.time.Clock()
-textinput.cursor_width = 0
-pygame.key.set_repeat(200, 25)
+user_text = ''
+input_rect = pygame.Rect(15,288,244,39)
+colour = (170,170,170)
 
-def getinputtext():
+# keeping track of room for game loop
+room = "play_game"
 
-	#setting event/updating if event
-	global events
-	events = pygame.event.get()
-	textinput.update(events)
-
-	#blitting typed text to screen
-	screen.blit(textinput.surface, (17, 290, 140, 32))
-
-	for event in events:
-		
-		#quitting if x pressed
-		if event.type == pygame.QUIT:
-			exit()
-
-		#clearing if enter pressed
-		elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN and textinput.value != "":
-			global textoutput
-			textoutput = textinput.value
-			textinput.value = ""
-	
-			screen.blit(bg_surface, (0, 0))
-
-		#removing last letter if backspace pressed
-		elif event.type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE and textinput.value != "":
-			screen.blit(bg_surface, (0, 0))
-
-	pygame.display.update()		
-	clock.tick(30)
-
-	#Rendering text
-def write(text):
-	
-	font = pygame.font.SysFont("dejavusans", 9)  
-	
- 	#Clearing the previous text
-	screen.blit(bg_surface, (0, 0))
-
- 	#setting position of each line
-	y = 45
-	x = 20
-
- 	#Splitting text iinto lines so next bit works
-	lines = text.split('\n')
-
-	#Rendering each line
-	for line in lines:
-		text_surface = font.render(line, True, (0, 0, 0))
-		screen.blit(text_surface, (x, y))
-		y += 9
-		pygame.display.update()
-		
 #setting inventory
 global inventory
 inventory = []
@@ -109,103 +55,37 @@ m_office_time = 0
 global lobby_time
 lobby_time = 0
 
-def play_game():
+#Rendering text
+def write(text):
+	
+	font = pygame.font.SysFont("dejavusans", 9)  
 
-	while True:
-		#setting input box to work
-		getinputtext()
-		global textoutput
-		play = textoutput
+ 	#setting position of each line
+	y = 45
+	x = 20
 
-		write("""Welcome to Down 
-to Freedom, would 
-you like to begin 
-your adventure?""")
+ 	#Splitting text iinto lines so next bit works
+	lines = text.split('\n')
 
-		# Removing spaces and upper cases.
-		play = play.strip().lower()
-		
-		if play == 'yes'or play == 'y':
-			textoutput = ""
-			write("""Within this 
-adventure your 
-keyboard is your guide. 
-
-To journey from room to 
-room use one of the
-following commands. 
-            
-  - To venture westward, type 
-    "Move West". 
-  - For a southern path, enter 
-    "Move South". 
-  - To tread northward, simply 
-    type 
-    "Move North". 
-  - For an eastward direction, 
-    type 
-    "Move East".""")
-
-    	#waiting for key press to move on
-			waiting_for_keypress = True
-			while waiting_for_keypress:
-				for event in pygame.event.get():
-					if event.type == pygame.KEYDOWN:
-						waiting_for_keypress = False
-
-			write("""Furthermore, to pick 
-up an item, use the 
-command:"Pick up" 
-followed by the name 
-of the item as it is 
-described in the 
-description of the room.
-
-In your journey through this text 
-adventure, items will prove their 
-worth without your explicit 
-command. When you work to 
-move in the correct direction, 
-carrying the fitting items in 
-your inventory, their usage will 
-come into play seamlessly.""")
-
-				#waiting for key press to move on
-			waiting_for_keypress = True
-			while waiting_for_keypress:
-				for event in pygame.event.get():
-					if event.type == pygame.KEYDOWN:
-						waiting_for_keypress = False
-			break
-
-		elif play == 'n' or play == 'no':
-			textoutput = ""
-			write("""You gaze into the 
-light as your one 
-chance of escaping 
-leaves.""")
-
-  		#waiting for key press to move on
-			pygame.time.wait(4000)
-
-    		#Quitting game
-			pygame.quit()
-			sys.exit()
-
-
-def meeting_room(inventory):
+	# Rendering each line
+	for line in lines:
+		story_surface = font.render(line, True, (0, 0, 0))
+		screen.blit(story_surface, (x, y))
+		y += 9
+		pygame.display.update()
+			
+	
+# meeting_room
+def meeting_room():
 
 	screen.blit(meeting_room_map, (158,20))
-
-	getinputtext()
-	global textoutput
 		
-	#varible used to determine if player has been in room before
+	# varible used to determine if player has been in room before
 	global meeting_time
 	if meeting_time == 0:
-		textoutput = ""
 		
   	#text for entering room first time
+		screen.blit(bg_surface, (0, 0))
 		write("""You come to find 
 yourself in a dimly 
 lit meeting room. Tall, 
@@ -234,70 +114,14 @@ card's presence.
   	
 		meeting_time = 1
 
-#Alternative text when entering room for second time
+	#	Alternative text when entering room for second time
 	else:
-		textoutput = ""
+		screen.blit(bg_surface, (0, 0))
 		write("""You have just 
 entered the Meeting 
 Room, Nothing has 
 changed since you 
 were last in here.""")
-
-	while True:
-		
-		#setting input box to work
-		getinputtext()
-		movement = textoutput
-		movement = movement.strip().lower()
-
-   	#attempting to move an invalid direction
-		if movement == "move north" or movement == "move south" or movement == "move west":
-			textoutput = ""
-			write("""As much as you may 
-wish to go in that 
-direction, there is 
-no door there.""")
-
- 		#attempting to leave without a keycard.
-		elif movement == "move east" and "red_keycard" not in inventory:
-			textoutput = ""
-			write("""You walk over to
-the door, ignoring 
-the keypad and 
-attempt to pull open 
-the door. Despite your 
-best efforts it refuses 
-to budge, giving up, you 
-look around the room 
-for another solution.""")
-
-	#Picking up keycard
-		elif movement == "pickup red keycard":
-			textoutput = ""
-			inventory.append("red_keycard")
-			screen.blit(r_keycard, (193, 240))
-			write("""You slide the red 
-keycard off the table 
-and into your hand 
-disturbing the dust as 
-you pick it up and place 
-it in your pocket for future 
-use.""")
-
- 		#moving to reception when requirements met
-		elif movement == "move east" and "red_keycard" in inventory:
-			textoutput = ""
-			reception(inventory)
-
-			#invailid input
-		elif movement not in ['move east', 'move west', 'move south', 'move north', 'pickup red keycard', '']:
-			textoutput = ""
-			write("""You look around 
-confused as to what 
-you were trying to do.""")
-			
-		pygame.display.update()
-	return inventory
 
 
 #Defining reception
@@ -305,11 +129,11 @@ def reception(inventory):
 
 	screen.blit(reception_map, (158,20))
 
-	global textoutput
+
  	#varible to determine if player has been in room before
 	global reception_time
 	if reception_time == 0:
-		textoutput = ""
+
 
    	#Text for entering room for first time
 		write("""After using the red 
@@ -341,7 +165,7 @@ off-white of each wall:
 
   	#Alternative text when entering room for second time
 	else:
-		textoutput = ""
+
 		write("""You have just 
 entered the 
 Reception, Nothing 
@@ -350,33 +174,31 @@ you were last here.""")
 
 	while True:
 		#setting input box to work
-		getinputtext()
-		movement = textoutput
+
 		movement = movement.strip().lower()
 
   	#Moveing to closet
 		if movement == "move south":
-			textoutput = ""
+
 			closet(inventory)
 
     	#Moveing to lobby
 		elif movement == "move east":
-			textoutput = ""
+
 			lobby(inventory)
 
    	#moving back to meeting room
 		elif movement == "move west":
-			textoutput = ""
+
 			meeting_room(inventory)
 
    	#moving to the office
 		elif movement == "move north":
-			textoutput = ""
+
 			office(inventory)
 
    	#invailid input
 		elif movement not in ['move east', 'move west', 'move south', 'move north', '']:
-			textoutput = ""
 			write("""You look around 
 confused as to what 
 you were trying to do.""")
@@ -389,12 +211,11 @@ def closet(inventory):
 
 	screen.blit(closet_map, (158,20))
 
-	global textoutput
+
 		
  	#Determining if player has been in room before
 	global closet_time
 	if closet_time == 0:
-		textoutput = ""
 
    	#text for entering room for first time.
 		write("""A faint scent of 
@@ -426,7 +247,7 @@ adventure.""")
 
  	#Alternative text when entering room for second time.
 	else:
-		textoutput = ""
+
 		write("""You have just entered 
 the Janitors Closet, 
 Nothing has changed 
@@ -435,13 +256,12 @@ in here.""")
 
 	while True:
 		#setting input box to work
-		getinputtext()
-		movement = textoutput
+
 		movement = movement.strip().lower()
 
    	#attempting to move an invalid direction
 		if movement == "move east" or movement == "move south" or movement == "move west":
-			textoutput = ""
+
 			write("""As much as you may 
 wish to go in that 
 direction, there is 
@@ -449,12 +269,12 @@ no door there.""")
 
    	#Moving back to reception
 		elif movement == "move north":
-			textoutput = ""
+
 			reception(inventory)
 
    	#picking up crowbar
 		elif movement == "pickup crowbar":
-			textoutput = ""
+
 			inventory.append("crowbar")
 			screen.blit(crowbar, (193, 130))
 			write("""Reaching down you 
@@ -469,7 +289,7 @@ adventure. """)
 
    	#incorrect input
 		elif movement not in ['move east', 'move west', 'move south', 'move north', 'pickup crowbar','']:
-			textoutput = ""
+
 			write("""You look around 
 confused as to what 
 you were trying to do.""")
@@ -483,12 +303,11 @@ def office(inventory):
 
 	screen.blit(office_map, (158,20))
 	
-	global textoutput
 		
 	#been in room before?
 	global office_time
 	if office_time == 0:
-		textoutput = ""
+
 
    	#entering room for first time
 		write("""Shoving the door 
@@ -519,7 +338,7 @@ of office life.""")
 
  	#Alternative text wehn entering room not for first time
 	else:
-		textoutput = ""
+
 		write("""You have just 
 entered the Office, 
 Nothing has changed 
@@ -528,13 +347,12 @@ here.""")
 
 	while True:
 		#setting input box to work
-		getinputtext()
-		movement = textoutput
+
 		movement = movement.strip().lower()
 
 		#attempting to move in invalid direction
 		if movement == "move north" or movement == "move east":
-			textoutput = ""
+
 			write("""As much as you may 
 wish to go in that 
 direction, there is 
@@ -542,17 +360,17 @@ no door there.""")
 
   	#moveing to managers office
 		elif movement == "move west":
-			textoutput = ""
+
 			m_office(inventory)
 
    	#moveing back to reception
 		elif movement == "move south":
-			textoutput = ""
+
 			reception(inventory)
 
    	#invalid input
 		elif movement not in ['move east', 'move west', 'move south', 'move north', '']:
-			textoutput = ""
+
 			write("""You look around 
 confused as to what 
 you were trying to do.""")
@@ -565,12 +383,12 @@ def m_office(inventory):
 
 	screen.blit(m_office_map, (158,20))
 	
-	global textoutput
+
 		
  	#Varible to determind if player been in roome before
 	global m_office_time
 	if m_office_time == 0:
-		textoutput = ""
+
 
    	#enterng room for first time
 		write("""Pushing the door 
@@ -600,7 +418,7 @@ on the corner of the desk.""")
 
 #entering room for secnd time
 	else:
-		textoutput = ""
+
 		write("""You have just 
 entered the Manager's 
 Office, nothing has 
@@ -609,13 +427,12 @@ last in here.""")
 
 	while True:
 		#setting input box to work
-		getinputtext()
-		movement = textoutput
+
 		movement = movement.strip().lower()
 
    	#attempting to move in invalid direction
 		if movement == "move north" or movement == "move south" or movement == "move west":
-			textoutput = ""
+
 			write("""As much as you may 
 wish to go in that 
 direction, there is 
@@ -623,12 +440,12 @@ no door there.""")
 
    	#Moving back to office
 		elif movement == "move east":
-			textoutput = ""
+
 			office(inventory)
 
    	#Picking up keycard
 		elif movement == "pickup orange keycard":
-			textoutput = ""
+
 			inventory.append("orange_keycard")
 			screen.blit(o_keycard, (193, 200))
 			write("""Carefully avoiding 
@@ -641,7 +458,7 @@ be useful in the future.""")
 
    	#incorrect input
 		elif movement not in ['move east', 'move west', 'move south', 'move north', 'pickup orange keycard','']:
-			textoutput = ""
+
 			write("""You look around 
 confused as to what 
 you were trying to do.""")
@@ -654,11 +471,11 @@ def lobby(inventory):
 	
 	screen.blit(lobby_map, (158,20))
 	
-	global textoutput
+
 	#has player been in roome before
 	global lobby_time
 	if lobby_time == 0:
-		textoutput = ""
+
 
    	#first time in room
 		write("""You stand in a 
@@ -690,7 +507,7 @@ glow.""")
 		lobby_time = 1
 
 	else:
-		textoutput = ""
+
 		write("""You have just 
 entered the
 Elevator Lobby, 
@@ -700,12 +517,11 @@ in here.""")
 
 	while True:
 		#setting input box to work
-		getinputtext()
-		movement = textoutput
+
 		movement = movement.strip().lower()
 
 		if movement == "move south" or movement == "move north" or movement == "move east":
-			textoutput = ""
+
 			write("""As much as you may 
 wish to go in that 
 direction, there is 
@@ -713,11 +529,11 @@ no door there.""")
 
    	#Meving back to reception
 		elif movement == "move west":
-			textoutput = ""
+ 
 			reception(inventory)
 
 		elif "orange_keycard" in inventory and "crowbar" in inventory:
-			textoutput = ""
+
 			write("""Anticipation hangs 
 in the air as you 
 stand in the elevator 
@@ -753,7 +569,7 @@ confines. """)
 
     	#invailid input
 		elif movement not in ['move east', 'move west', 'move south', 'move north', '']:
-			textoutput = ""
+
 			write("""You look around 
 confused as to what 
 you were trying to do.""")
@@ -761,24 +577,171 @@ you were trying to do.""")
 		pygame.display.update()
 	return inventory
 
-
-# Game not started (Making sure bg appears first)
-game_started = False
-
-# Game loop
 while True:
-	if not game_started:
-   	# Making sure bg appears first
-  	#pygame.display.update()
-		game_started = True
-		screen.blit(bg_surface, (0, 0))
+	
+	# taking input from input box/ making it work
+	for event in pygame.event.get():
+		
+		if event.type == pygame.QUIT:				
+			pygame.quit()
+			sys.exit()
 
-		#setting up text input
-		getinputtext()
+		elif event.type == pygame.KEYDOWN:
+			
+			if event.key == pygame.K_BACKSPACE:
+				user_text = user_text[:-1]
+				
+			elif event.key == pygame.K_RETURN:
+				input = ""
+				user_text = user_text.strip().lower()
+				input = user_text
+				user_text = ""
+				
+			else:
+				user_text += event.unicode
+				if text_surface.get_width() > input_rect.w -20:
+					user_text = user_text[:-1]
+		
+		# would you like to play the game/intro
+		if room == "play_game":
+			screen.blit(bg_surface, (0, 0))
+			write("""Welcome to Down 
+to Freedom, would 
+you like to begin your 
+adventure?""")
 
- 		#Begining the loop of def functions
-		play_game()
-		meeting_room(inventory)
+			if input == 'yes'or input == 'y':
+				screen.blit(bg_surface, (0, 0))
+				write("""Within this 
+adventure your 
+keyboard is your guide. 
 
-  		#Update the screen
-		pygame.display.update()
+To journey from room to 
+room use one of the
+following commands. 
+
+- To venture westward, type 
+  "Move West". 
+- For a southern path, enter 
+  "Move South". 
+- To tread northward, simply 
+  type 
+  "Move North". 
+- For an eastward direction, 
+  type 
+  "Move East".
+ 
+(Press any key to continue)""")
+
+				#waiting for key press to move on
+				waiting_for_keypress = True
+				while waiting_for_keypress:
+					for event in pygame.event.get():
+						if event.type == pygame.KEYDOWN:
+							waiting_for_keypress = False
+
+				screen.blit(bg_surface, (0, 0))
+				write("""Furthermore, to pick 
+up an item, use the 
+command:"Pick up" 
+followed by the name 
+of the item as it is 
+described in the 
+description of the room.
+
+In your journey through this text 
+adventure, items will prove their 
+worth without your explicit 
+command. When you work to 
+move in the correct direction, 
+carrying the fitting items in 
+your inventory, their usage will 
+come into play seamlessly.
+
+(Press any key to continue)""")
+
+				# waiting for key press to move on
+				waiting_for_keypress = True
+				while waiting_for_keypress:
+					for event in pygame.event.get():
+						if event.type == pygame.KEYDOWN:
+							waiting_for_keypress = False
+
+				room = "meeting_room"
+
+			elif input == 'n' or input == 'no':
+				screen.blit(bg_surface, (0, 0))
+				write("""You gaze into the 
+light as your one 
+chance of escaping 
+leaves.""")
+
+				# waiting for user to read
+				pygame.time.wait(4000)
+
+				# Quitting game
+				pygame.quit()
+				sys.exit()
+			
+	# Meeting room
+	if room == "meeting_room":
+		meeting_room()
+		
+		# attempting to move an invalid direction
+		if input == "move north" or input == "move south" or input == "move west":
+
+			screen.blit(bg_surface, (0, 0))
+			write("""As much as you may 
+wish to go in that 
+direction, there is 
+no door there.""")
+
+	# attempting to leave without a keycard.
+		elif input == "move east" and "red_keycard" not in inventory:
+
+			screen.blit(bg_surface, (0, 0))
+			write("""You walk over to
+the door, ignoring 
+the keypad and 
+attempt to pull open 
+the door. Despite your 
+best efforts it refuses 
+to budge, giving up, you 
+look around the room 
+for another solution.""")
+
+		# Picking up keycard
+		elif input == "pickup red keycard":
+
+			inventory.append("red_keycard")
+			screen.blit(r_keycard, (193, 240))
+			screen.blit(bg_surface, (0, 0))
+			write("""You slide the red 
+keycard off the table 
+and into your hand 
+disturbing the dust as 
+you pick it up and place 
+it in your pocket for future 
+use.""")
+
+		# moving to reception when requirements met
+		elif input == "move east" and "red_keycard" in inventory:
+			room = "reception"
+
+		# invailid input
+		elif input not in ['move east', 'move west', 'move south', 'move north', 'pickup red keycard', '']:
+
+			screen.blit(bg_surface, (0, 0))
+			write("""You look around 
+confused as to what 
+you were trying to do.""")
+
+
+	pygame.draw.rect(screen,colour,input_rect)
+	
+	text_surface = font.render(user_text, True, (0,0,0))
+	screen.blit(text_surface,(input_rect.x + 5, input_rect.y + 7))
+  #Update the screen
+	pygame.display.update()
+
+	clock.tick(60)
